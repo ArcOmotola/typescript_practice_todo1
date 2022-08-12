05bab1d6dde398fde8e7a55af59404c83b8fda1c
Omotola Ogunsola <ogunsola40@gmail.com>
2022-08-12 08:04:24 +0100

    styled single card

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Button, Card } from 'react-bootstrap';
 import { Note } from "../models/note.model";
 
 interface INotesProps {
@@ -7,7 +8,16 @@ interface INotesProps {
 
 const Notes: React.FC<INotesProps> = ({note}) => {
   return (
-    <div>Note Card</div>
+    <div className='mb-3'>
+        <Card style={{backgroundColor: note.color}}>
+            <Card.Body>
+                <Card.Title>{note.title}</Card.Title>
+                <Card.Title>{note.text}</Card.Title>
+                <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+                <Button className='mt-3' variant='danger'>Delete</Button>
+            </Card.Body>
+        </Card>
+    </div>
   );
 };
 
