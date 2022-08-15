ce648900f1257476f6bb7b32eeec38b242a77a09
Omotola Ogunsola <ogunsola40@gmail.com>
2022-08-15 16:03:31 +0100

    added submit function

--- src/components/CreateNotes.tsx
+++ src/components/CreateNotes.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Note } from '../models/note.model';
 import { Button, Form } from 'react-bootstrap';
 
@@ -8,14 +8,31 @@ interface ICreateNotesProps {
 }
 
 const CreateNotes: React.FunctionComponent<ICreateNotesProps> = ({notes, setNotes}) => {
+    const [error, setError] = useState<string>("")
     const titleRef = useRef<HTMLInputElement | null>(null);
     const textRef = useRef<HTMLTextAreaElement | null>(null);
     const colorRef = useRef<HTMLInputElement | null>(null);
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        if(titleRef.current?.value === "" || textRef.current?.value === "") {
+            return setError("All fields are mandatory");
+        }
+
+        setError("");
+        setNotes([...notes, {
+            id: (new Date()).toString(),
+            title: (titleRef.current as HTMLInputElement).value,
+            text: (textRef.current as HTMLTextAreaElement).value,
+            color: (colorRef.current as HTMLInputElement).value,
+            date: (new Date()).toString()
+        }])
+    }
+
     return (
         <>
             <h2>Create Notes</h2>
-            <Form className="mt-3 mb-3">
+            <Form className="mt-3 mb-3" onSubmit={(e) => handleSubmit(e)}>
                 <Form.Group className='mb-3' controlId='formBasic'>
                     <Form.Label>Title</Form.Label>
                     <Form.Control type='text' placeholder='Enter Title for the Note' ref={ titleRef }/>
